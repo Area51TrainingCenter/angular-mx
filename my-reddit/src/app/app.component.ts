b9f92625a17355a06d7e51bb426325b23cc543b2
yacaFx <sergiobritor@gmail.com>
2017-07-08 15:54:18 -0500

    Set model logic

--- my-reddit/src/app/app.component.ts
+++ my-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  name = 'Sergio';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 5),
+      new Article('Semantic', 'https://semantic-ui.com/', 8),
+      new Article('Bootstrap', 'https://getbootstrap.com/', 4)
+    ]
+  }
 }
