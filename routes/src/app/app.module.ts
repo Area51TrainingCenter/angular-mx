fad6c218f017283d9574108b953d248ff00269ef
yacaFx <sergiobritor@gmail.com>
2017-07-22 14:59:16 -0500

    Login

--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -1,24 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { 
+import {
   RouterModule,
   Routes
 } from '@angular/router'
+import {
+  LocationStrategy,
+  HashLocationStrategy
+} from '@angular/common'
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { P404Component } from './p404/p404.component';
+import { LoginComponent } from './login/login.component';
+import { AUTH_PROVIDERS } from './auth.service';
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contacto', redirectTo: 'contact' },
+  { path: '**', component: P404Component },
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    P404Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot()
+    RouterModule.forRoot(routes)
 
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS
+    //{ provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
