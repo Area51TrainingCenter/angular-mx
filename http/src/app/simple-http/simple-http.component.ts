95b96d08cbf77c96243c026e559208ad087de312
yacaFx <sergiobritor@gmail.com>
2017-07-22 13:41:46 -0500

    htpp

--- http/src/app/simple-http/simple-http.component.ts
+++ http/src/app/simple-http/simple-http.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import {
+  Http,
+  Response,
+  Headers,
+  RequestOptions
+} from '@angular/http';
 
 @Component({
   selector: 'app-simple-http',
@@ -13,20 +18,73 @@ export class SimpleHttpComponent implements OnInit {
   constructor(private http: Http) { }
 
   ngOnInit() {
-    
+    // this.makeRequest()
+  }
+
+  makeHeaders() {
+    const headers: Headers = new Headers();
+    headers.append('x-access-token', '1234567890');
+    headers.append('myCors', '*');
+
+    const opts: RequestOptions = new RequestOptions();
+    opts.headers = headers;
+
+    this.http.get('http://jsonplaceholder.typicode.com/posts/1', opts)
+      .subscribe(
+      result => {
+        this.data = result.json();
+        this.loading = false;
+      }
+      )
+  }
+
+  makeDelete(): void {
+    this.loading = true;
+    this.http
+      .delete(
+      'http://jsonplaceholder.typicode.com/posts/1'
+      )
+      .subscribe(
+      result => {
+        this.data = result.json();
+        this.loading = false;
+      },
+      error => { console.log(error) }
+      )
+  }
+
+  makePost(): void {
+    this.loading = true;
+    this.http
+      .post(
+      'http://jsonplaceholder.typicode.com/posts',
+      JSON.stringify({
+        body: 'bar',
+        title: 'Mi titulo',
+        userId: 3
+      })
+      )
+      .subscribe(
+      result => {
+        this.data = result.json();
+        this.loading = false;
+      },
+      error => { console.log(error) }
+      )
   }
 
   makeRequest(): void {
     this.loading = true;
     this.http
       .request('http://jsonplaceholder.typicode.com/posts/1')
+      //.get('http://jsonplaceholder.typicode.com/posts/1')
       .subscribe(
       result => {
         this.data = result.json();
         this.loading = false;
       },
-      error => { console.log(error) },
-    )
+      error => { console.log(error) }
+      )
   }
 
 }
